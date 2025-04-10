2d4ea1bba7ed7294ba181f2d5131c7e286a55d8b
Emmanuel Malik <emmanuel00440@gmail.com>
2025-04-10 10:09:59 +0100

    js done for mobile

--- scripts/main.js
+++ scripts/main.js
@@ -63,6 +63,64 @@ buttonElement.addEventListener('click', () => {
   validEmail();
 })
 
+emailElement.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    //checking if email is empty
+    if (!emailElement.value || typeof emailElement.value !== 'string') {
+      displayError();
+      return;
+    }
+
+    //making sure there is only one @ symbol
+    const atSymbolCount = countCharacter(emailElement.value, '@');
+    if (atSymbolCount !== 1) {
+      displayError();
+      return;
+    }
+
+    //validating username and domain parts
+    const parts = emailElement.value.split('@');
+    const username = parts[0];
+    const domain = parts[1];
+
+    if (username.length === 0 || domain.length === 0) {
+      displayError();
+      return;
+    }
+
+    //make sure domain has at least one dot
+    const dotCount = countCharacter(domain, '.');
+    if (dotCount < 1) {
+      displayError();
+      return;
+    }
+
+    //make sure domain doesn't end with a dot
+    if (domain.endsWith('.')) {
+      displayError();
+      return;
+    }
+
+    //check if domain ends with at least one letter
+    const domainParts = domain.split('.');
+    const lastPartDomain = domainParts[domainParts.length - 1];
+    if (lastPartDomain.length < 2) {
+      displayError();
+      return;
+    }
+
+    const validCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.-_@';
+    for (let i = 0; i < emailElement.value.length; i++) {
+      if (!validCharacters.includes(emailElement.value[i])) {
+        displayError();
+        return;
+      }
+    }
+
+    validEmail();
+  }
+});
+
 function displayError() {
   errorTextElement.innerHTML = '';
 
@@ -90,6 +148,9 @@ function validEmail() {
 
   errorTextElement.classList.add('display-thank-you-text');
 
+  setTimeout(() => {
+    errorTextElement.innerHTML = '';
+  }, 3000);
 
 }
 
